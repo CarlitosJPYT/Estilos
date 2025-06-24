54187f98662c9152a2771d3494a657dde07f1dfd
Carlitos <juanpana10ospina@gmail.com~>
2025-06-24 18:54:03 -0500

    gitlens

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const fugue = {
+    "Fugue" : {
+    "Exposicion" : {
+      "Tema" : "T",
+      "ContraTema" : ["CT1", "CT2"],
+      "Entrada" : ["Complementaria", "EC"]
+    },
+    "Episodio" : "",
+    "Desarrollo" : {
+      "Tema" : "T",
+      "ContraTema" : ["CT1", "CT2"]
+    },
+    "Re_Exposicion" : {
+      "Tema" : "T",
+      "ContraTema" : ["CT1", "CT2"]
+    },
+    "Coda" : "C"
+  }
+}
